7d7d6ebbf24260dab3fd2757df946c496702c5eb
simon <sitiu@deloitte.com>
2014-06-10 11:45:17 -0600

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name editDistanceProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the editDistanceProjectApp
+ */
+angular.module('editDistanceProjectApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
